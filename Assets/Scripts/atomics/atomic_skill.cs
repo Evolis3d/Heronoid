384d1bf47ef1fc39bb54aebfaa8f3a0d5c3dbcc0
Evolis3d <programacion@evolis3d.com>
2021-02-08 19:52:58 +0100

    1er intento skills
    implementando skills: WIP

---
+++ Assets/Scripts/atomics/atomic_skill.cs
@@ -0,0 +1,81 @@
+﻿//clase generica que define un skill base.
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace mierdergames.atomics
+{
+
+    #region clase base
+
+    [Serializable]
+    public abstract class BaseSkill
+    {
+        [SerializeField] public string name;
+        [SerializeField] public bool enabled;
+
+        protected BaseSkill(string nam)
+        {
+            name = nam;
+            enabled = false;
+        }
+
+        protected BaseSkill()
+        {
+            name = "test skill";
+            enabled = false;
+        }
+
+        public virtual void ApplySkill()
+        {
+            enabled = true;
+        }
+    }
+    #endregion
+
+    #region skill pasiva
+    
+    [Serializable]
+    public class PassiveSkill : BaseSkill
+    {
+        [SerializeField] public bool isTogglable; //se puede desactivar cuando se quiera?
+
+        public PassiveSkill(string nam, bool istoggle = false)
+        {
+            name = nam;
+            enabled = false;
+            isTogglable = istoggle;
+        }
+
+        public void ToggleSkill()
+        {
+            if (isTogglable) enabled = !enabled;
+        }
+    }
+    #endregion
+
+    #region skill de personajito
+    
+    [Serializable]
+    public class PersonajitoSkill : BaseSkill
+    {
+        public override void ApplySkill()
+        {
+            enabled = true;
+            //la pala o lo que toque...
+        }
+    }
+    #endregion
+
+    #region skill consumible
+
+    public class ConsumibleSkill : BaseSkill
+    {
+        [SerializeField] public bool isOnlyOnce;
+    }
+    #endregion
+    
+
+
+}
